e1f99bba336939d79e701f6ac30319f16c28d2d0
Vladislav Horbenko <gorbenko@dnt-lab.com>
2020-02-06 07:13:28 +0200

    BWS-002 created map Brewery, with modify files

--- Breweries/Breweries/Brewery.swift
+++ Breweries/Breweries/Brewery.swift
@@ -7,3 +7,62 @@
 //
 
 import Foundation
+
+
+struct Brewery: Codable {
+    var id        : Int?
+    var name      : String?
+    var type      : String?
+    var street    : String?
+    var city      : String?
+    var state     : String?
+    var postalCode: String?
+    var country   : String?
+    var longitude : String?
+    var latitude  : String?
+    var phone     : String?
+    var website   : String?
+    
+    init(id: Int?,
+         name: String?,
+         type: String?,
+         street: String?,
+         city: String?,
+         state: String?,
+         postalCode: String?,
+         country: String?,
+         longitude: String?,
+         latitude: String?,
+         phone: String?,
+         website: String?) {
+        
+        self.id = id
+        self.name = name
+        self.type = type
+        self.street = street
+        self.city = city
+        self.state = state
+        self.postalCode = postalCode
+        self.country = country
+        self.longitude = longitude
+        self.latitude = latitude
+        self.phone  = phone
+        self.website = website
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case type       = "brewery_type"
+        case street
+        case city
+        case state
+        case postalCode = "postal_code"
+        case country
+        case longitude
+        case latitude
+        case phone
+        case website    = "website_url"
+    }
+    
+}
